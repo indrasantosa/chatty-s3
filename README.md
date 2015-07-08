c7b37f1ed39afce0e2b70db4be4a62d239998708
Indra Santosa <indrasantosa90@gmail.com>
2015-07-08 23:34:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is chatty-s3
+A lambda script to notify external web hooks when an s3 bucket getting some
+action or changes
---
+++ index.js
@@ -0,0 +1,37 @@
+console.log('Loading function');
+
+var aws = require('aws-sdk');
+var request = require('superagent');
+var s3 = new aws.S3({ apiVersion: '2006-03-01' });
+var config = require('./config')
+
+
+exports.handler = function(event, context) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    // Get the object from the event and show its content type
+		var region = event.Records[0].awsRegion;
+    var bucket = event.Records[0].s3.bucket.name;
+    var key = event.Records[0].s3.object.key;
+
+		var notifyUrl = config.notifyUrl;
+
+		// Define data that you want to send to the other side
+		var objectPublicUrl = 'http://'+ region + '.' + 'amazonaws.com/' + bucket + '/' + key;
+		var data = {
+			status: 'success',
+			data: {
+				publicUrl : objectPublicUrl,
+				metadata : event.Records[0]
+			}
+		}
+
+		// Log to AWS before sending for debuggin purpose
+		console.log('Sending to ', notifyUrl, ' data ', data);
+
+		request.post(notifyUrl)
+	    .set('Content-Type', 'application/json')
+	    .send(data)
+	    .end()
+
+};
